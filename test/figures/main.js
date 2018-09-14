8b656547a3089e481db528d518309d887302ca84
dehui.kdh <dehui.kdh@alibaba-inc.com>
2018-09-14 15:26:23 +0800

    add custom domain support and fix bad case

--- test/figures/main.js
+++ test/figures/main.js
@@ -2,22 +2,22 @@
 
 var getRawBody = require('raw-body');
 
-exports.handler = function(event, context, callback) {
+exports.handler = function (event, context, callback) {
   console.log('event: %s', event.toString());
-  callback(null, 'hello '+event.toString());
+  callback(null, 'hello ' + event.toString());
 };
 
-exports.http_handler = function(request, response, context) {
-  getRawBody(request, function(err, body) {
+exports.http_handler = function (request, response, context) {
+  getRawBody(request, function (err, body) {
     console.log('request: url:%s, path:%s, method:%s, clientIP:%s, body:%s',
-            request.url, request.path, request.method, request.clientIP, body.toString());
+      request.url, request.path, request.method, request.clientIP, body.toString());
     console.log('header:%s\nqueries:%s', JSON.stringify(request.headers), JSON.stringify(request.queries));
     var retBody = {
-        headers: request.headers,
-        queries: request.queries,
-        url: request.url,
-        clientIP: request.clientIP,
-        method: request.method,
+      headers: request.headers,
+      queries: request.queries,
+      url: request.url,
+      clientIP: request.clientIP,
+      method: request.method,
     };
     for (var key in request.queries) {
       if (request.queries.hasOwnProperty(key)) {
